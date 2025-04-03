b8a7e7561628537b426bad5c903b6655ca0ff8c3
atish waghmare <atish234@gmail.com>
2025-04-03 12:40:29 +0530

    diff add successfully

---
+++ diff.java
@@ -0,0 +1,12 @@
+public class Subtraction {
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+        int result = subtract(num1, num2);
+        System.out.println("The result of subtraction is: " + result);
+    }
+}
